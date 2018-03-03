39184dffb8175d15a3bb38eb9413a1a396bde677
jaredramirez <jaredramirez@me.com>
2018-03-03 16:17:15 -0600

    Add cli parsing

--- src/Main.hs
+++ src/Main.hs
@@ -3,44 +3,85 @@
 module Main where
 
 import Data.Semigroup ((<>))
-
-{- import qualified Data.Version as Version -}
-{- import qualified Paths_teleport as Teleport -}
-import qualified Options.Applicative as Opt
+import qualified Data.Version as Version
+import qualified Options.Applicative as OptApplicative
 import Options.Applicative ((<**>))
+import qualified Paths_teleport as Teleport
 
-data Sample = Sample
-  { hello :: String
-  , quiet :: Bool
-  , enthusiasm :: Int
+-- COMMANDS
+data Command
+  = Add String
+  | Warp String
+  | Remove String
+
+warpPointArg :: OptApplicative.Parser String
+warpPointArg =
+  OptApplicative.strArgument
+    (OptApplicative.idm <> OptApplicative.metavar "WARP_POINT")
+
+createCommand ::
+     String
+  -> OptApplicative.Parser cmd
+  -> String
+  -> OptApplicative.Mod OptApplicative.CommandFields cmd
+createCommand commandName commandArgParser commandDescription =
+  OptApplicative.command
+    commandName
+    (OptApplicative.info
+       commandArgParser
+       (OptApplicative.progDesc commandDescription))
+
+commandOption :: OptApplicative.Parser Command
+commandOption =
+  OptApplicative.subparser $
+  createCommand
+    "add"
+    (fmap Add warpPointArg)
+    "Add a warp point to the current directory" <>
+  createCommand
+    "warp"
+    (fmap Warp warpPointArg)
+    "Warp to the specified warp point" <>
+  createCommand
+    "rm"
+    (fmap Remove warpPointArg)
+    "Remove the specified warp point"
+
+-- OPTIONS
+data Options = Options
+  { command :: Command
+  , version :: Bool
   }
 
-sample :: Opt.Parser Sample
-sample =
-  Sample <$>
-  Opt.strOption
-    (Opt.long "hello" <> Opt.metavar "TARGET" <>
-     Opt.help "Target for the greeting") <*>
-  Opt.switch
-    (Opt.long "quiet" <> Opt.short 'q' <> Opt.help "Whether to be quiet") <*>
-  Opt.option
-    Opt.auto
-    (Opt.long "enthusiasm" <> Opt.help "How enthusiastically to greet" <>
-     Opt.showDefault <>
-     Opt.value 1 <>
-     Opt.metavar "INT")
+versionSwitch :: OptApplicative.Parser Bool
+versionSwitch =
+  OptApplicative.switch $
+  OptApplicative.long "version" <> OptApplicative.short 'v' <>
+  OptApplicative.help "Version of Teleport"
 
-greet :: Sample -> IO ()
-greet s =
-  case s of
-    (Sample h False n) -> putStrLn $ "Hello " ++ h ++ replicate n '!'
-    _ -> return ()
+options :: OptApplicative.Parser Options
+options = Options <$> commandOption <*> versionSwitch
 
+info :: OptApplicative.ParserInfo Options
+info =
+  OptApplicative.info
+    (options <**> OptApplicative.helper)
+    (OptApplicative.fullDesc <>
+     OptApplicative.progDesc
+       "Save & teleport to differnt locations on your filesystem" <>
+     OptApplicative.header "Teleport CLI tool")
+
+-- MAIN ROUTINE
+mainRoutine :: Options -> IO ()
+mainRoutine a =
+  case a of
+    (Options _ True) -> putStrLn $ Version.showVersion Teleport.version
+    (Options cmd False) ->
+      case cmd of
+        Add name -> putStrLn $ "adding current directory " ++ name
+        Warp _ -> putStrLn "warping"
+        Remove _ -> putStrLn "remove"
+
+-- RUNNER
 main :: IO ()
-main = greet =<< Opt.execParser opts
-  where
-    opts =
-      Opt.info
-        (sample <**> Opt.helper)
-        (Opt.fullDesc <> Opt.progDesc "Print a greeting for TARGET" <>
-         Opt.header "hello - a test for optparse-applicative")
+main = mainRoutine =<< OptApplicative.execParser info
